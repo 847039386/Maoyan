99d346fc6d2b166e915e25a6008257dc694edcb1
847039386 <ai847039386@vip.qq.com>
2017-06-21 15:30:51 +0800

    .

--- bin/util/Util.ts
+++ bin/util/Util.ts
@@ -92,13 +92,10 @@ export class Util {
     getAgentList(url : string){
         return new Promise((resolve,reject) => {
             superAgent("get",url)
-                .set('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.104 Safari/537.36 Core/1.53.2372.400 QQBrowser/9.5.10548.400')
                 .end((err :any,data :any) => {
                     if(err){
                         resolve(null)
                     }else{
-                        console.log(data.text)
-                        console.log(url)
                         resolve(JSON.parse(data.text))
                     }
                 })
